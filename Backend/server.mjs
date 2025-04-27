ce4f0bc809cd2fbe721d1bd145668c1998f25c42
ALIZAIQBAL <alizaiqbal322@gmail.com>
2025-04-27 07:58:41 +0500

    update

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -10,7 +10,18 @@ connectToDB();
 
 const app = express();
 //Middleware
-app.use(cors());
+app.use(
+	cors({
+		origin: ['http://localhost:5174',
+			 'http://localhost:5173',
+			 'employee-management-system-xg36.vercel.app',
+			
+			],
+		methods: ['GET', 'POST'],
+		credentials: true,
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	}),
+);
 app.use(express.json());
 
 //Routes
